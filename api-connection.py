a00e2c8a94fcc37df4342f9d119e1787488ce97c
Guhan Iyer <80014275+guuuuie@users.noreply.github.com>
2022-07-22 20:19:41 -0400

    Create api-connection.py

---
+++ api-connection.py
@@ -0,0 +1,13 @@
+import json
+
+file = open(f"{path}\\Chess Moves.pgn", "r")
+
+read = file.read()
+
+file.close()
+
+response = requests.post("https://lichess.org/api/import",  
+    data = {"pgn": read}
+)
+
+print(response.json()["url"])
