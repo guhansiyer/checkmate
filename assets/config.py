f7b31a1d6285a872e286e9041098a11aa41646e1
Guhan Iyer <guhan.shyam.iyer05@gmail.com>
2022-07-29 17:45:10 -0400

    Created "imgMask()" function to crop the chess board out of real photos

--- assets/config.py
+++ assets/config.py
@@ -1,3 +1,3 @@
 # path = "C:\\Users\\Antonio\\Desktop\\Python Programs\\Chess"
-# path = "C:\\Users\\guhan\\Desktop\\checkmate"
-path = "C:\\Users\\guhan\\Pictures"
+path = "C:\\Users\\guhan\\Desktop\\checkmate"
+# path = "C:\\Users\\guhan\\Pictures"
--- assets/mask.py
+++ assets/mask.py
@@ -1,13 +1,28 @@
 import cv2 as cv
 import numpy as np 
 import config
+from time import sleep
 
-img = cv.imread("C:\\Users\\guhan\\Desktop\\checkmate\\newChessPNGs\\realChessPNGs (coloured)\\Move2.png")
-rsz_img = cv.resize(img, None, fx=0.20, fy=0.20)
-mask = np.zeros(rsz_img.shape[:2], dtype = "uint8")
-mask = cv.rectangle(mask, (115,225), (500, 600), (255,255,255), -1)
+def imageMask(num):
+    img = cv.imread(f"{config.path}\\newChessPNGs\\realChessPNGs (coloured)\\Move" + str(num) + ".png")
+    rsz_img = cv.resize(img, None, fx=0.20, fy=0.20)
 
-result = cv.bitwise_and(rsz_img, rsz_img, mask = mask)
+    mask = np.zeros(rsz_img.shape[:2], dtype = "uint8")
+    mask = cv.rectangle(mask, (115,225), (500, 600), (255,255,255), -1)
+
+    result = cv.bitwise_and(rsz_img, rsz_img, mask = mask)
+    
+    cv.imwrite(f"{config.path}\\cutPhotos\\Move" + str(num) + ".png", result)
+    """ cv.imshow("Masked Move " + str(num), result)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+ """
+
+for i in range(36):
+    imageMask(i)
+    print('Image cut')
+    sleep(1)
+    
 #Standard Thresholding
 #ret, thresh_gray = cv.threshold(gray, 150, 255, cv.THRESH_BINARY)
 
@@ -15,7 +30,7 @@ result = cv.bitwise_and(rsz_img, rsz_img, mask = mask)
 #ret, thresh_gray = cv.threshold(gray, 150, 55, cv.THRESH_BINARY_INV)
 
 #Otsu's Method
-#ret, thresh_gray = cv.threshold(gray, 150, 55, cv.THRESH_BINARY+cv.THRESH_OTSU)
+#ret, thresh_gray = cv.threshold(gray, 150, 55, cv.THRESH_BINARY + cv.THRESH_OTSU)
 
 #Adaptive Mean Thresholding
 #thresh_gray = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, \
@@ -24,9 +39,3 @@ result = cv.bitwise_and(rsz_img, rsz_img, mask = mask)
 #Adaptive Gaussian Thresholding 
 #thresh_gray = cv.adaptiveThreshold(gray, 220, cv.ADAPTIVE_THRESH_GAUSSIAN_C, \
            #cv.THRESH_BINARY, 11, 2)
-
-print(rsz_img.shape)
-cv.imshow("Masked", result)
-cv.waitKey(0)
-
-cv.destroyAllWindows()
