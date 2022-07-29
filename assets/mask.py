b863803a54a54a74b96526e39e0773e020554e16
Guhan Iyer <guhan.shyam.iyer05@gmail.com>
2022-07-29 16:40:19 -0400

    Thresholding tool redeveloped to perform image masking

--- assets/mask.py
+++ assets/mask.py
@@ -2,11 +2,10 @@ import cv2 as cv
 import numpy as np 
 import config
 
-img = cv.imread(f"{config.path}\\Move0.png")
+img = cv.imread("C:\\Users\\guhan\\Desktop\\checkmate\\newChessPNGs\\realChessPNGs (coloured)\\Move2.png")
 rsz_img = cv.resize(img, None, fx=0.20, fy=0.20)
-#gray = cv.cvtColor(rsz_img, cv.COLOR_BGR2GRAY)
 mask = np.zeros(rsz_img.shape[:2], dtype = "uint8")
-mask = cv.rectangle(mask, (150,150), (450, 450), 255, -1)
+mask = cv.rectangle(mask, (115,225), (500, 600), (255,255,255), -1)
 
 result = cv.bitwise_and(rsz_img, rsz_img, mask = mask)
 #Standard Thresholding
@@ -27,7 +26,7 @@ result = cv.bitwise_and(rsz_img, rsz_img, mask = mask)
            #cv.THRESH_BINARY, 11, 2)
 
 print(rsz_img.shape)
-cv.imshow("Thresholded", result)
+cv.imshow("Masked", result)
 cv.waitKey(0)
 
 cv.destroyAllWindows()
