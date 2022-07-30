5f70fd5ed8a6161504a4a6cef7d159d4c2bb4956
Guhan Iyer <guhan.shyam.iyer05@gmail.com>
2022-07-30 13:08:10 -0400

    Created nwe set of real photos for testig

--- assets/crop.py
+++ assets/crop.py
@@ -16,6 +16,7 @@ def imageCrop(num):
 
     #Performing a bitwise AND operation to combine the original photo onto the mask
     result = cv.bitwise_and(rsz_img, rsz_img, mask = mask)
+
     #img_gray = cv.cvtColor(result, cv.COLOR_BGR2GRAY)
     #contours = cv.findContours(img_gray, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
     #cv.drawContours(rsz_img, contours, -1, (0,255,0), 3)
@@ -26,12 +27,11 @@ def imageCrop(num):
     (topx, topy) = (np.min(x), np.min(y))
     #Finding the bottom right corner of the contour
     (bottomx, bottomy) = (np.max(x), np.max(y))
-    #Finding the area enclosed by the two corners
+    #Cropping the area enclosed by the corners
     img_final = result[topx:bottomx+1, topy:bottomy+1]
 
     #Writing the output image to a folder
     cv.imwrite(f"{config.path}\\cutPhotos\\Move" + str(num) + ".png", img_final)
-
 
     #cv.imwrite(f"{config.path}\\cutPhotos\\Move" + str(num) + ".png", result)
     #cv.imshow("Masked Move " + str(num), rsz_img)
@@ -39,9 +39,6 @@ def imageCrop(num):
     #cv.destroyAllWindows()
     
 
-for i in range(34):
-    imageCrop(i)
-    sleep(0.5)
 
 #Standard Thresholding
 #ret, thresh_gray = cv.threshold(gray, 150, 255, cv.THRESH_BINARY)
