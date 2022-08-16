6b30730f8d4cda6e717bd4ff63d2de632def97ab
Guhan Iyer <guhan.shyam.iyer05@gmail.com>
2022-08-15 20:35:40 -0400

    New file

--- assets/generic_crop.py
+++ assets/generic_crop.py
@@ -3,6 +3,7 @@ import cv2 as cv
 import numpy as np 
 
 def crop(num):
-    image = cv.imread(f"C:\\Users\\guhan\\Desktop\\chess\\realChessPNGs (coloured)\\Move" + str(num) + ".png")
+    image = cv.imread(f"C:\\Users\\guhan\\Desktop\\chess\\realChessPNGs (coloured)\\Move" + str(num) + ".png")d
+    
     
   
